0645aa590200da9eabb47660f9935b095e0d3629
Petrnov321 <98708359+Petrnov321@users.noreply.github.com>
2022-06-08 19:27:18 +0200

    Initial commit

---
+++ src/com/engeto/euvatrates/Main.java
@@ -0,0 +1,16 @@
+package com.engeto.euvatrates;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            String s = String.valueOf(i);
+            if ((i%3 == 0) && (i%5 == 0)) {    s = "CracklePop";    }
+            else if (i%3 == 0){    s = "Crackle";    }
+            else if (i%5 == 0) {     s = "Pop";     }
+
+            System.out.println(s);
+        }
+    }
+}
